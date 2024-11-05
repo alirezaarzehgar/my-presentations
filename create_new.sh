4a13fa5559cf853d0cc173bd8e6f7d35f154df17
alirezaarzehgar <alirezaarzehgar82@gmail.com>
2024-11-05 22:38:28 +0330

    maint: add some default command to create_new.sh
    Signed-off-by: alirezaarzehgar <alirezaarzehgar82@gmail.com>

--- create_new.sh
+++ create_new.sh
@@ -19,9 +19,26 @@ __EOF__
 
 cat << __EOF__ > "$pres_name/$pres_name.tex"
 \documentclass[xcolor={rgb}]{beamer}
+\usepackage[labelformat=empty]{caption}
 
 \newcommand{\pitem}{\pause\item}
 \usetheme{cfermat}
+
+\newcommand{\img}[3]{
+	\only<#1>{
+		\begin{figure}
+			\includegraphics[width=\textwidth]{#2}
+			\caption{#3}
+		\end{figure}
+	}
+}
+
+\newcounter{_i}
+\newcommand{\starcounting}{\setcounter{_i}{2}}
+\newcommand{\simg}[2]{
+	\stepcounter{_i}
+	\img{\value{_i}}{#1}{#2}
+}
 
 \author{Alireza Arzehgar}
 \title{Title}
